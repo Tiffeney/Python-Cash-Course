49c21093ce9c36c395fcad355b04d00f143eb4d9
Tiffeney <tiffeney.rambo@gmail.com>
2019-10-22 21:08:03 -0700

    created a new file * used data type numbers

---
+++ try_it_uself_2.py
@@ -0,0 +1,15 @@
+#Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations
+#in print statements to see the results. You should create four lines that look like this:
+#print(5 + 3)
+
+print( 2 + 2 + 2 + 2)
+print( 2 * 4)
+print( 10 - 2)
+print( 2 ** 3)
+
+#Store your favorite number in a variable. Then, using the variable, create a message that reveals your favorite number. Print the message
+favorite_number = 13
+print("my favorite number is " + str(favorite_number) + "!")
+
+#Learn about import this
+
