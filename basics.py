7aafb9aacf8b603d99106acf11faaa6a4cdc7631
Tiffeney <tiffeney.rambo@gmail.com>
2019-10-22 20:16:56 -0700

    learn how to start on a new line, tab, & remove whitespace

--- basics.py
+++ basics.py
@@ -1,25 +1,42 @@
-name = "ada lovelace"
-print(name)
+# name = "ada lovelace"
+# print(name)
 
-#Method title() will capitalize every letter where each word begins with a capital letter. 
-print(name.title())
+# #Method title() will capitalize every letter where each word begins with a capital letter. 
+# print(name.title())
 
-full_name = "tiffeney elizaeth ariel rambo"
-print(full_name.title())
-print(full_name.lower())
-print(full_name.upper())
+# full_name = "tiffeney elizaeth ariel rambo"
+# print(full_name.title())
+# print(full_name.lower())
+# print(full_name.upper())
 
 
-#Combine both stings name & full_name
-print(name + full_name)
+# #Combine both stings name & full_name
+# print(name + full_name)
 
-first_name = 'tiffeney'
-last_name = 'rambo'
-middle_name = 'elizabeth ariel'
-full_name = first_name + middle_name + last_name
+# first_name = 'tiffeney'
+# last_name = 'rambo'
+# middle_name = 'elizabeth ariel'
+# full_name = first_name + middle_name + last_name
 
-print(full_name)
+# print(full_name)
 
-#Noticing the spacing? How do you fix it?
-full_name = first_name + " " + middle_name + " " + last_name
-print(full_name)
+# #Noticing the spacing? How do you fix it?
+# full_name = first_name + " " + middle_name + " " + last_name
+# print(full_name)
+
+# #concatenating
+# print("my full government name is " + first_name + " " + middle_name + " " + last_name + " but please call me just tiffeney or tiff!")
+
+
+# #Use tab & new line
+# print("\n\twhat is your name?")
+
+# print("my full government name is " + first_name + " " + middle_name + " " + last_name + " but please call me just tiffeney or tiff!")
+
+
+moms_name = " Demeta "
+print(moms_name)
+
+moms_name = moms_name.lstrip()
+print(moms_name)
+print(moms_name)
