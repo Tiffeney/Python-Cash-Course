fc34c87b77c02a112d6e46be365ecc125aea115d
Tiffeney <tiffeney.rambo@gmail.com>
2019-10-22 21:25:37 -0700

    created a new file and learned about 'list'

--- try_it_yourself/try_it_urself_3.py
+++ try_it_yourself/try_it_urself_3.py
@@ -1,9 +1,18 @@
 #3-1. Names: Store the names of a few of your friends in a list called names . Print each person’s name by accessing each element in the list, 
 # one at a time.
 
+best_friends = ["whiteney", "asia", "betsy", "meshia", "kristi"]
+print(best_friends[0])
+print(best_friends[1])
+print(best_friends[2])
+print(best_friends[3])
+print(best_friends[4])
 
 #3-2. Greetings: Start with the list you used in Exercise 3-1, but instead of just printing each person’s name, print a message to them . 
 # The text of each message should be the same, but each message should be personalized with the person’s name.
+
+print(best_friends[0] + 'is my sister/bestfreind')
+
 
 
 
