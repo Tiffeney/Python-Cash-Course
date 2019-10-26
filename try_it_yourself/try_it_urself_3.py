a3029b0e6db2ca99873d6d2e9f92d5522397f496
Tiffeney <tiffeney.rambo@gmail.com>
2019-10-26 15:31:57 -0700

    trying to figure out what to do next

--- try_it_yourself/try_it_urself_3.py
+++ try_it_yourself/try_it_urself_3.py
@@ -66,6 +66,11 @@ print(places_to_visit)
 #3-9. Dinner Guests: Working with one of the programs from Exercises 3-4 through 3-7 (page 46), use len() to print a message indicating the 
 # number of people you are inviting to dinner .
 
+print(len(places_to_visit))
+
 
 #3-10. Every Function: Think of something you could store in a list . For example, you could make a list of mountains, rivers, countries, 
-#cities, languages, or any- thing else you’d like . Write a program that creates a list containing these items and then uses each function introduced in this chapter at least once .
+#cities, languages, or any- thing else you’d like . Write a program that creates a list containing these items and then uses each 
+# function introduced in this chapter at least once.
+
+
